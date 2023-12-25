56d5187b380ff3acec9edd436e1d614b65fda95d
usfree <tlrmsjtm77@naver.com>
2023-12-25 18:37:38 +0900

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -104,4 +104,150 @@
 */
 })(document.documentElement);
 
+var get_ocid;
+var api_key = "test_02623554ef843dbcfc123dd4da641b021390670490c415561c29676c50ef5a4fb1cf4e9d24e21ddee71643e81e700659";
+let today = new Date();
+let year = today.getFullYear(); // 년도
+let month = today.getMonth() + 1;  // 월
+let date = today.getDate() - 1;  // 날짜
+var today_date = year + '-' + month + '-' + date;
+
+function chrsearch(){
+    $.ajax({
+            method: "GET",
+            url: "https://open.api.nexon.com/maplestory/v1/id",
+            data: {
+                character_name: input_name,
+            },
+            headers: {
+                "x-nxopen-api-key": api_key
+            },
+            success: function (data) {
+                get_ocid = data.ocid;
+                console.log(data.ocid);
+            },
+            complete: function() {
+                $.ajax({
+                    method: "GET",
+                    url: "https://open.api.nexon.com/maplestory/v1/character/basic",
+                    data: {
+                        ocid: get_ocid,
+                        date: today_date
+                    },
+                    headers: {
+                        "x-nxopen-api-key": api_key
+                    },
+                })
+                .done(function (msg) {
+                    console.log(msg);
+                    $('.character_name').empty().append(msg.character_name);
+                    $('.world_name').empty().append(msg.world_name);
+                    $('.character_level').empty().append(msg.character_level);
+                    $('.character_class').empty().append(msg.character_class);
+                    $('.character_guild_name').empty().append(msg.character_guild_name);
+                    $('.character_image').empty().append('<img src="' + msg.character_image + '" style="width: 115%;"/>');
+                    $('.character_exp_rate').empty().append(msg.character_exp_rate); 
+                });
+                $.ajax({
+                    method: "GET",
+                    url: "https://open.api.nexon.com/maplestory/v1/character/dojang",
+                    data: {
+                        ocid: get_ocid,
+                        date: today_date
+                    },
+                    headers: {
+                        "x-nxopen-api-key": api_key
+                    },
+                })
+                .done(function (msg) {
+                    console.log(msg);
+                    $('.dojang_best_floor').empty().append(msg.dojang_best_floor + '층');
+                    $('.dojang_best_time').empty().append(Math.floor(msg.dojang_best_time / 60) + '분');
+                    $('.dojang_best_time_sec').empty().append(msg.dojang_best_time % 60 + '초');
+                });
+                $.ajax({
+                    method: "GET",
+                    url: "https://open.api.nexon.com/maplestory/v1/character/popularity",
+                    data: {
+                        ocid: get_ocid,
+                        date: today_date
+                    },
+                    headers: {
+                        "x-nxopen-api-key": api_key
+                    },
+                })
+                .done(function (msg) {
+                    console.log(msg);
+                    $('.popularity').empty().append(msg.popularity);
+                });
+                $.ajax({
+                    method: "GET",
+                    url: "https://open.api.nexon.com/maplestory/v1/user/union",
+                    data: {
+                        ocid: get_ocid,
+                        date: today_date
+                    },
+                    headers: {
+                        "x-nxopen-api-key": api_key
+                    },
+                })
+                .done(function (msg) {
+                    console.log(msg);
+                    $('.union_level').empty().append(msg.union_level);
+                    $('.union_grade').empty().append(msg.union_grade);
+                });
+                $.ajax({
+                    method: "GET",
+                    url: "https://open.api.nexon.com/maplestory/v1/ranking/achievement",
+                    data: {
+                        ocid: get_ocid,
+                        date: today_date
+                    },
+                    headers: {
+                        "x-nxopen-api-key": api_key
+                    },
+                })
+                .done(function (msg) {
+                    console.log(msg);
+                    $('.trophy_score').empty().append(msg.ranking[0].trophy_score);
+                    $('.trophy_grade').empty().append(msg.ranking[0].trophy_grade);
+                });
+                $.ajax({
+                    method: "GET",
+                    url: "https://open.api.nexon.com/maplestory/v1/ranking/theseed",
+                    data: {
+                        ocid: get_ocid,
+                        date: today_date
+                    },
+                    headers: {
+                        "x-nxopen-api-key": api_key
+                    },
+                })
+                .done(function (msg) {
+                    $('.theseed_floor').empty().append(msg.ranking[0].theseed_floor + '층');
+                    $('.theseed_time_record').empty().append(Math.floor(msg.ranking[0].theseed_time_record / 60) + '분');
+                    $('.theseed_time_record_sec').empty().append(msg.ranking[0].theseed_time_record % 60 + '초');
+                });
+                $.ajax({
+                    method: "GET",
+                    url: "https://open.api.nexon.com/maplestory/v1/character/stat",
+                    data: {
+                        ocid: get_ocid,
+                        date: today_date
+                    },
+                    headers: {
+                        "x-nxopen-api-key": api_key
+                    },
+                })
+                .done(function (msg) {
+                    $('.stat_value42').empty().append(msg.final_stat[42].stat_value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    $('.stat_value0').empty().append(msg.final_stat[0].stat_value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    $('.stat_value1').empty().append(msg.final_stat[1].stat_value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                });
+            }
+        })
+
+        
+    }
+
 
